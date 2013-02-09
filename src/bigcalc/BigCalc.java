7a75c41bb7eecb28086734e49e4454c76aa0da7e
bittugoyal700 <bittugoyal7000@gmail.com>
2013-02-09 23:44:27 +0530

    Initial commit

---
+++ src/bigcalc/BigCalc.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bigcalc;
+
+import UI.Home;
+
+/**
+ *
+ * @author unicorn
+ */
+public class BigCalc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Home().setVisible(true);
+    }
+}
